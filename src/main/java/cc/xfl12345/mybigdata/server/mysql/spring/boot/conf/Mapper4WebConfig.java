29200efe5e18ae02b01f16c72aeaabf053094c64
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-09-19 13:46:24 +0800

    跟进依赖 server-mysql 到 Git 版本 0c40ccb5c7 ，注册更多 Mapper 。

--- src/main/java/cc/xfl12345/mybigdata/server/mysql/spring/boot/conf/Mapper4WebConfig.java
+++ src/main/java/cc/xfl12345/mybigdata/server/mysql/spring/boot/conf/Mapper4WebConfig.java
@@ -1,9 +1,14 @@
 package cc.xfl12345.mybigdata.server.mysql.spring.boot.conf;
 
 import cc.xfl12345.mybigdata.server.common.web.mapper.AccountMapper;
+import cc.xfl12345.mybigdata.server.common.web.mapper.AdvanceSearchMapper;
+import cc.xfl12345.mybigdata.server.common.web.mapper.DatabaseViewer;
 import cc.xfl12345.mybigdata.server.mysql.database.converter.AppIdTypeConverter;
 import cc.xfl12345.mybigdata.server.mysql.database.mapper.AuthAccountMapper;
+import cc.xfl12345.mybigdata.server.mysql.database.mapper.base.impl.CoreTableCache;
 import cc.xfl12345.mybigdata.server.mysql.mapper.AccountMapperImpl;
+import cc.xfl12345.mybigdata.server.mysql.mapper.AdvanceSearchMapperImpl;
+import cc.xfl12345.mybigdata.server.mysql.mapper.DatabaseViewerImpl;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +16,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @AutoConfigureAfter({MapperConfig.class})
-public class AccountMapper4WebConfig {
+public class Mapper4WebConfig {
     @Bean
     @ConditionalOnMissingBean
     public AccountMapper accountMapper(AuthAccountMapper mapper, AppIdTypeConverter idTypeConverter) {
@@ -20,5 +25,17 @@ public class AccountMapper4WebConfig {
         accountMapper.setIdTypeConverter(idTypeConverter);
 
         return accountMapper;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public AdvanceSearchMapper advanceSearchMapper(CoreTableCache coreTableCache) {
+        return new AdvanceSearchMapperImpl();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public DatabaseViewer databaseViewer(CoreTableCache coreTableCache) throws Exception {
+        return new DatabaseViewerImpl();
     }
 }
--- src/main/java/cc/xfl12345/mybigdata/server/mysql/spring/boot/conf/OrmRelatedConfig.java
+++ src/main/java/cc/xfl12345/mybigdata/server/mysql/spring/boot/conf/OrmRelatedConfig.java
@@ -1,6 +1,8 @@
 package cc.xfl12345.mybigdata.server.mysql.spring.boot.conf;
 
+import cc.xfl12345.mybigdata.server.common.web.mapper.AdvanceSearchMapper;
 import cc.xfl12345.mybigdata.server.mysql.database.mapper.base.impl.CoreTableCache;
+import cc.xfl12345.mybigdata.server.mysql.mapper.AdvanceSearchMapperImpl;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -15,5 +17,11 @@ public class OrmRelatedConfig {
     @ConditionalOnMissingBean
     public CoreTableCache coreTableCache(SessionFactory sessionFactory) {
         return new CoreTableCache();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public AdvanceSearchMapper advanceSearchMapper(CoreTableCache coreTableCache) {
+        return new AdvanceSearchMapperImpl();
     }
 }
